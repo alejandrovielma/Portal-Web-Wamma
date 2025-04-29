a7915c5c9c3fe05d5537ec1df7c9ac8a234a2c7a
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-04-29 07:06:18 -0400

    refactor: dependencias taildwind y gridstack

---
+++ src/page/home/Home.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from 'react'
+import { GridStack } from 'gridstack';
+import './Home.css'
+
+function Home() {
+
+  return (
+    <>
+        <h1>GridStack</h1>
+        
+    </>
+  );
+}
+
+export default Home;
