76f2839f2f1c61de3448f23d310097b41a9aac33
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-05-07 08:59:38 -0400

    feat: detectar eventos de DnD

--- src/pages/library/Library.tsx
+++ src/pages/library/Library.tsx
@@ -1,6 +1,58 @@
+import { ComponentDataType, ComponentMap, GridStackProvider, GridStackRender, GridStackRenderProvider } from "#lib/gridStackLib/index.ts";
+import { GridStackOptions } from "gridstack";
+import { ComponentProps, useEffect, useState } from "react";
+
+
+function Text({ content }: { content: string }) {
+  return <div className="w-full h-full">{content}</div>;
+}
+
+const COMPONENT_MAP: ComponentMap = {
+  Text,
+  // ... other components here
+};
+
+const gridOptions: GridStackOptions = {
+  acceptWidgets: true,
+  float: true,
+  column: 1,
+  row: 1,
+  disableResize: true,
+  margin:0,
+  children: [
+    {
+      id: "item1",
+      h: 1,
+      w: 1,
+      x: 0,
+      y: 0,
+      content: JSON.stringify({
+        name: "Text",
+        props: { content: "Item 1" },
+      } satisfies ComponentDataType<ComponentProps<typeof Text>>), // if need type check
+    }
+  ],
+};
+
 export function Library() {
+    const [initialOptions] = useState(gridOptions);
+
+    function handleEvent(event: Event) {
+        console.log("Event triggered:", event);
+    }
+
     return (
-        <div>Library</div>
+        <>
+            
+            <div className="size-60">
+                
+                <GridStackProvider initialOptions={initialOptions}>
+                    <GridStackRenderProvider onEvent={handleEvent}>
+                    <GridStackRender componentMap={COMPONENT_MAP} />
+                    </GridStackRenderProvider>
+                </GridStackProvider>
+            </div>
+        </>
     )
 }
 export default Library;
