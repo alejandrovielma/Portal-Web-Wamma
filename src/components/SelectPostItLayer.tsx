e349f6d51e25b95766e428efe75170f5558fa310
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-05-10 20:10:07 -0400

    rebert: volver a SelectPostItLayer con GridStackProvider como receptor

--- src/components/SelectPostItLayer.tsx
+++ src/components/SelectPostItLayer.tsx
@@ -1,22 +1,42 @@
-import { ReactNode } from "react"
+import { COMPONENT_MAP, GridStackProvider, GridStackRender, GridStackRenderProvider } from "#lib/gridStackLib/index.ts"
+import { ReactNode, useState } from "react"
+import { GridStackOptions } from "gridstack";
 import { useNavigate } from "react-router-dom";
 import { navigateTransitionToMenu } from "#components/TransitionToMenuButton.tsx";
 
+const gridOptions: GridStackOptions = {
+  acceptWidgets: true,
+  float: true,
+  column: 1,
+  row: 1,
+  disableResize: true,
+  margin:0,
+  children: [
+  ],
+};
+
 export function SelectPostItLayer({  children, isLargePadding }: { children?: ReactNode | undefined, isLargePadding: boolean }) {
+    const [initialOptions] = useState(gridOptions);
     const navigate = useNavigate();
 
-    const onMouseEnter = () => {
-        navigateTransitionToMenu(navigate, "/")
+    function handleEvent(event: Event) {
+        console.log(event);
+        if (event.type === 'added') {
+            //navigateTransitionToMenu(navigate, '/');
+        }
     }
     
     return (
         <>
             <div
-            className="bg-red-500 overflow-hidden transition-all duration-500 delay-200"
+            className="overflow-hidden transition-all duration-500 delay-200"
             style={{ height: isLargePadding ? '10rem' : '0'}}
-            onMouseEnter={onMouseEnter}
             >
-                
+                <GridStackProvider initialOptions={initialOptions}>
+                    <GridStackRenderProvider onEvent={handleEvent}>
+                    <GridStackRender componentMap={COMPONENT_MAP} />
+                    </GridStackRenderProvider>
+                </GridStackProvider>
             </div>
             <div className="pageContainer">
                 {children}
