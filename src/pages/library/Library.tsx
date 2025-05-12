a1c3014967d0fab3b2097769c254fa54f9a39820
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-05-12 11:20:35 -0400

    refactor: Library componente navheader

--- src/pages/library/Library.tsx
+++ src/pages/library/Library.tsx
@@ -1,47 +1,22 @@
 import {
   BreadcrumbItem,
-  generateBreadcrumbs,
   NavHeader,
 } from "#components/NavHeader.tsx";
-import PopInfo from "#components/PopInfo.tsx";
 import SelectPostItLayer from "#components/SelectPostItLayer.tsx";
 import UnitPostIt from "#components/UnitPostIt.tsx";
 import { useState } from "react";
 
 export function Library() {
-  // Estado para el manejo de la ventana emergente
-  const [isPopOpen, setIsPopOpen] = useState(true);
-
-  // Estado para el manejo del arrastre
   const [isDragging, setIsDragging] = useState(false);
   function handleEvent(event: Event) {
     setIsDragging(event.type === "dragstart");
   }
 
-  const pageTitle = "Inicio"; // Título de la página
-
-  // Estructura de breadcrumbs original
-  const libraryBreadcrumbs: BreadcrumbItem[] = [
-    { nombre: "Biblioteca", link: "/biblioteca" },
-  ];
-
-  // Generación de breadcrumbs dinámicos
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
-  const pathSegments = currentPath
-    .split("/")
-    .filter((segment) => segment !== "");
-  const breadcrumbs: BreadcrumbItem[] = generateBreadcrumbs(pathSegments);
-
   return (
     <SelectPostItLayer isDragging={isDragging}>
       <NavHeader
-        titulo={pageTitle}
-        pathItems={libraryBreadcrumbs}
-        breadcrumbs={breadcrumbs}
+        pathItems={[{ nombre: "Biblioteca", link: "/biblioteca" }]}
       />
-
-      {/* Integración de PopInfo */}
-      {isPopOpen && <PopInfo onClose={() => setIsPopOpen(false)} />}
 
       <div className="size-60">
         <UnitPostIt imageLink="images/homeBg.jpg" handleEvent={handleEvent} />
