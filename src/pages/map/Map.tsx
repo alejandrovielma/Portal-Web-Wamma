d924ff08fcca593f171e58aa284bb39d18ad6935
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-05-14 16:51:26 -0400

    feat: inicio del mapa

--- src/pages/map/Map.tsx
+++ src/pages/map/Map.tsx
@@ -2,6 +2,7 @@ import {NavHeader} from "#components/NavHeader.tsx";
 import SelectPostItLayer from "#components/SelectPostItLayer.tsx";
 import UnitPostIt from "#components/UnitPostIt.tsx";
 import { useState } from "react";
+import { MapContainer, TileLayer } from "react-leaflet"
 
 export function Map() {
   const [isDragging, setIsDragging] = useState(false);
@@ -14,6 +15,12 @@ export function Map() {
       <NavHeader
         pathItems={[{ nombre: "Mapa", link: "/mapa" }]}
       />
+      <MapContainer center={[8.332897505049878, -62.67421343794216]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </SelectPostItLayer>
   );
 }
