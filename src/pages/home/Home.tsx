eb6d2190815b1eb79b8b4ac8ea0aaabe72931365
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-05-14 14:08:19 -0400

    feat: Quitar menu desplegable de home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,41 +2,17 @@ import NavMenu from "#components/NavMenu.js";
 import SearchBar from "#components/SearchBar.tsx";
 import { TransitionToPage } from "#components/TransitionToPage.tsx";
 import GlobalGrip from "#components/globalgrid/GlobalGrid.tsx";
-import { useState } from "react";
 
 export function Home() {
-  const [menu, setMenu] = useState<{ x: number; y: number } | null>(null);
-
-  const handleContextMenu = (event: React.MouseEvent) => {
-    event.preventDefault();
-    setMenu({ x: event.clientX, y: event.clientY });
-  };
-
-  const handleClose = () => setMenu(null);
 
   return (
-    <div className="relative min-h-screen" onContextMenu={handleContextMenu}>
+    <>
       <GlobalGrip/>
       <SearchBar/>
       <NavMenu text="Menu"/>
       <TransitionToPage/>
-      {menu && (
-        <>
-        {/* Overlay para cerrar el menú al hacer clic fuera */}
-        <div
-          className="fixed inset-0 z-40"
-          onClick={handleClose}
-        />
-        <div
-          className="absolute bg-white border rounded shadow p-2 z-50"
-          style={{ top: menu.y, left: menu.x }}
-        >
-          <button className="block w-full text-left">Opción 1</button>
-          <button className="block w-full text-left">Opción 2</button>
-        </div>
-      </>
-      )}
-    </div>
+      
+    </>
   );
 }
 export default Home;
