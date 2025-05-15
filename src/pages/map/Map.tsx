f9b80cbcc67bb7b9ca9255025fe7823fa7b70d4b
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-05-14 20:19:14 -0400

    feat: mapa funcional

--- src/pages/map/Map.tsx
+++ src/pages/map/Map.tsx
@@ -2,7 +2,8 @@ import {NavHeader} from "#components/NavHeader.tsx";
 import SelectPostItLayer from "#components/SelectPostItLayer.tsx";
 import UnitPostIt from "#components/UnitPostIt.tsx";
 import { useState } from "react";
-import { MapContainer, TileLayer } from "react-leaflet"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import "leaflet/dist/leaflet.css";
 
 export function Map() {
   const [isDragging, setIsDragging] = useState(false);
@@ -15,11 +16,17 @@ export function Map() {
       <NavHeader
         pathItems={[{ nombre: "Mapa", link: "/mapa" }]}
       />
-      <MapContainer center={[8.332897505049878, -62.67421343794216]} zoom={13}>
+      <MapContainer center={[8.332897505049878, -62.67421343794216]} zoom={13} className="w-full h-full z-0">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          className="w-screen h-screen"
         />
+        <Marker position={[8.332897505049878, -62.67421343794216]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
       </MapContainer>
     </SelectPostItLayer>
   );
