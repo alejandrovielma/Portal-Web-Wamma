78b41db1f7a6d744bb23fb91a893543ab6834fec
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-05-16 10:20:06 -0400

    feat: icono de abrir y cerrar cambiado en sliderMap

--- src/assets/CompressSVG.tsx
+++ src/assets/CompressSVG.tsx
@@ -1,7 +1,7 @@
 export function CompressSVG() {
     return (
         <svg className="w-6 h-6 text-shadow-50 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 8h4V4m12 4h-4V4M4 16h4v4m12-4h-4v4" />
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m9 5 7 7-7 7" />
         </svg>
     )
 }
--- src/assets/ExpandedSVG.tsx
+++ src/assets/ExpandedSVG.tsx
@@ -1,7 +1,7 @@
 export function ExpandedSVG() {
     return (
         <svg className="w-6 h-6 text-shadow-50 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 4H4m0 0v4m0-4 5 5m7-5h4m0 0v4m0-4-5 5M8 20H4m0 0v-4m0 4 5-5m7 5h4m0 0v-4m0 4-5-5"/>
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m15 19-7-7 7-7" />
         </svg>
     )
 }
