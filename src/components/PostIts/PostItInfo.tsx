8a87219285eb57d08f13ecb48e80fa59c0943a4e
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-05-15 22:16:35 -0400

    feat: api del clima y estilar PostItInfo

--- src/components/PostIts/PostItInfo.tsx
+++ src/components/PostIts/PostItInfo.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PostItBase from "./PostItBase";
 import DialogInfo from "#components/DialogInfo.tsx";
+import { useGridStackContext } from "#lib/gridStackLib/grid-stack-context.ts";
+import { GridStackWidget } from "gridstack";
+import { useGridStackWidgetContext } from "#lib/gridStackLib/grid-stack-widget-context.ts";
 
 export interface PostItInfoContent {
     subtitle?: string;
@@ -11,21 +14,42 @@ export interface PostItInfoProps {
     title: string
     content: PostItInfoContent[]
     video?: string
-    images?: string[]
+    images: string[]
 }
 
 export function PostItInfo({title, content, video, images }: PostItInfoProps) {
     const [isPopOpen, setIsPopOpen] = useState(false);
+    const { widget } = useGridStackWidgetContext();
+    const { gridStack, saveOptions } = useGridStackContext()
+    const [dimensions, setDimensions] = useState({ h: 2, w: 2 });
+    const [childrens, setChildrens] = useState<GridStackWidget[]>(saveOptions()["children"] as GridStackWidget[])
+
+    useEffect(() => {
+        if (!gridStack) return;
+        function updateDimensions() {
+            const self = childrens.find((child: GridStackWidget) => child.id === widget.id)
+            setDimensions({
+                h: self?.h || 2,
+                w: self?.w || 2,
+            })
+        }
+        updateDimensions();
+    }, [])
     
+    const allContent = {
+        title: title,
+        content: content,
+        video: video,
+        images: images
+    }
     return (
         <>  
             <PostItBase color1="bg-light-primary dark:bg-dark-primary" color2="bg-light-primaryVar dark:bg-dark-primaryVar">
                 <button onClick={()=>{setIsPopOpen(true)}} className="flex flex-col gap-2 w-full h-full cursor-pointer">
-                    {/* Mostrar solo la primera imagen y el título principal */}
-                    {images && images[0] && (
-                        <img src={images[0]} alt="" className="w-full h-56 object-cover rounded-t-2xl" />
-                    )}
-                    {title && <h2 className="text-center text-2xl font-bold py-4">{title}</h2>}
+                    {
+                    dimensions.h <= 2 && dimensions.w <= 2 ? <ContentSmall allContent={allContent} /> :
+                        <ContentMedium allContent={allContent} />
+                        }
                 </button>
             </PostItBase>
             <DialogInfo active={isPopOpen} onClose={()=>{setIsPopOpen(false)}}>
@@ -62,5 +86,19 @@ export function PostItInfo({title, content, video, images }: PostItInfoProps) {
         </>
     );
 }
-
 export default PostItInfo;
+
+function ContentSmall({ allContent }: { allContent: PostItInfoProps }) {
+    return <img className="flex-1" src={allContent.images[0]} alt={allContent.title} />
+}
+function ContentMedium({ allContent }: { allContent: PostItInfoProps }) {
+    return (
+        <>
+            <img className="flex-1" src={allContent.images[0]} alt={allContent.title} />
+            <div className="flex-1/2 px-4 py-2">
+                <h2 className="font-titles text-left text-xl" >{allContent.title}</h2>
+                <p className="text-left">{allContent.content[0]?.paragraphs?.join(' ')}</p>
+            </div>
+        </>
+    )
+}
--- src/components/PostIts/PostItMap.tsx
+++ src/components/PostIts/PostItMap.tsx
@@ -5,6 +5,7 @@ import { useGridStackWidgetContext } from "#lib/gridStackLib/grid-stack-widget-c
 import { useGridStackContext } from "#lib/gridStackLib/grid-stack-context.ts";
 import { useEffect, useState } from "react";
 import { GridStackWidget } from "gridstack";
+const WHEATHER_API = import.meta.env.VITE_WEATHER_API;
 
 export interface PostItMapProps {
     title: string;
@@ -23,10 +24,19 @@ export function PostItMap({ title, description, images, video, coordinates, city
     const { widget } = useGridStackWidgetContext();
     const { saveOptions, gridStack } = useGridStackContext()
     const [dimensions, setDimensions] = useState({ h: 2, w: 2 });
-    
+
+    const [weather, setWeather] = useState();
+
+    useEffect(() => {
+        fetch(`http://api.weatherapi.com/v1/current.json?key=${WHEATHER_API}&q=${city}&aqi=no`)
+            .then(response => response.json())
+            .then(json => { setWeather(json) })
+            .catch(error => console.error("Error:", error));
+    }, []);
 
 
     useEffect(() => {
+        if (!gridStack) return;
         function updateDimensions() {
             const childrens: GridStackWidget[] = saveOptions()["children"]
             const self = childrens.find((child: GridStackWidget) => child.id === widget.id)
@@ -70,8 +80,8 @@ export function PostItMap({ title, description, images, video, coordinates, city
             <button onClick={handleClick} className="flex flex-col gap-2 w-full h-full cursor-pointer">
                 {
                     dimensions.h <= 2 && dimensions.w <= 2 ? <ContentSmall content={content} /> :
-                        dimensions.h <= 4 && dimensions.w <= 4 ? <ContentMedium content={content} /> :
-                            <ContentLarge content={content} />
+                        dimensions.h <= 6 && dimensions.w <= 6 ? <ContentMedium content={content} /> :
+                            <ContentLarge content={content} weather={weather} />
                 }
 
             </button>
@@ -91,17 +101,25 @@ function ContentMedium({ content }: { content: PostItMapProps }) {
         </>
     )
 }
-function ContentLarge({ content }: { content: PostItMapProps }) {
-    return (
-        <>
-            <div>
-                <h2 className="font-titles text-left text-xl" >{content.title}</h2>
-                <img className="flex-1" src={content.images[0]} alt={content.title} />
-            </div>
-            <div>
+function ContentLarge({ content, weather }: { content: PostItMapProps, weather?: any }) {
 
+    return (
+        <div className="flex flex-col gap-2">
+            <h2 className="font-titles text-left text-2xl" >{content.title}</h2>
+            <div className="flex gap-2">
+                <div className="flex flex-1 overflow-hidden">
+                    <img className="h-full object-cover" src={content.images[0]} alt={content.title} />
+                </div>
+                <div className="flex flex-1 items-start justify-center">
+                    {weather && (
+                        <span className="flex flex-col justify-center items-center gap-2">
+                            <img className="size-16" src={weather["current"]["condition"]["icon"]} alt="condition" />
+                            <h3 className="text-xl">{weather["current"]["condition"]["text"]}</h3>
+                        </span>
+                    )}
+                </div>
             </div>
-        </>
+        </div>
 
     )
 }
