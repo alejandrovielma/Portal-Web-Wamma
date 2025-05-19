3228bbe0db4f345b3e06ad720131c29b5b7b4465
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-05-18 21:58:33 -0400

    fix: poner id para que cargen los postit

--- src/lib/gridStackLib/grip-stack-global-widget-provider.tsx
+++ src/lib/gridStackLib/grip-stack-global-widget-provider.tsx
@@ -7,7 +7,7 @@ import { image, video } from "framer-motion/client";
 import { getAllArticles } from "../../data/dataBase/repository";
 
 type WidgetContextType = {
-  widgets:  GridStackWidget[];
+  widgets: GridStackWidget[];
   setWidgets: (widgets: GridStackWidget[]) => void;
 };
 
@@ -15,7 +15,7 @@ const WidgetContext = createContext<WidgetContextType | undefined>(undefined);
 
 const WIDGETS_STORAGE_KEY = "gridstack-widgets";
 
-export function GridStackGlobalWidgetProvider({ children }:{ children: React.ReactNode }) {
+export function GridStackGlobalWidgetProvider({ children }: { children: React.ReactNode }) {
   const [widgets, setWidgetsState] = useState<GridStackWidget[]>(() => {
     //Carga los widgets en localStorage y si existen los usa
     const cached = localStorage.getItem(WIDGETS_STORAGE_KEY);
@@ -24,45 +24,26 @@ export function GridStackGlobalWidgetProvider({ children }:{ children: React.Rea
         return JSON.parse(cached);
       } catch (error) {
         console.error("Error parsing cached widgets:", error);
-        localStorage.removeItem(WIDGETS_STORAGE_KEY); 
+        localStorage.removeItem(WIDGETS_STORAGE_KEY);
       }
     }
 
-
     const articles = getAllArticles().slice(0, 4);
+
     const defaultWidgets: GridStackWidget[] = articles.map((article, index) => {
       const x = (index % 2) * 4;
       const y = Math.floor(index / 2) * 5;
-      const images = article.images && article.images.length > 0
-        ? article.images.map((img, i) => ({
-            url: img,
-            alt: `Image ${i + 1}`,
-          }))
-        : [];
-      const videos = article.video
-        ? [{ url: article.video, title: "Video" }]
-        : [];
-      const content = article.content.map(c => {
-        if (c.paragraphs) {
-          return { type: "paragraph", content: c.paragraphs.join("\n") };
-        } else if (c.subtitle) {
-          return { type: "subtitle", content: c.subtitle };
-        }
-        return {};
-      });
 
       return {
+        id: `itemDeafault${index}`,
         x: x,
         y: y,
-        w: 2,
-        h: 2,
+        w: 4,
+        h: 4,
         content: JSON.stringify({
           name: "PostItInfo",
           props: {
-            title: article.title,
-            content: content,
-            images: images,
-            video: videos,
+            ...article
           },
         } satisfies ComponentDataType<ComponentProps<typeof PostItInfo>>),
       };
@@ -71,8 +52,8 @@ export function GridStackGlobalWidgetProvider({ children }:{ children: React.Rea
   });
 
   useEffect(() => {
-    if (widgets.length > 0) { 
-        localStorage.setItem(WIDGETS_STORAGE_KEY, JSON.stringify(widgets));
+    if (widgets.length > 0) {
+      localStorage.setItem(WIDGETS_STORAGE_KEY, JSON.stringify(widgets));
     }
   }, [widgets]);
 
