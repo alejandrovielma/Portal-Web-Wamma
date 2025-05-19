b7d153b867459f31e06ef9982b70b9cba75d9a31
Bloodbay8 <153459461+Bloodbay8@users.noreply.github.com>
2025-05-19 09:34:59 -0400

    feat: Background en movimiento de Animales

--- src/pages/animals/Animals.tsx
+++ src/pages/animals/Animals.tsx
@@ -1,9 +1,9 @@
+import { useState, useEffect } from "react";
 import NavBubbleAnimals from "#components/NavBubbleAnimals.tsx";
 import { NavHeader } from "#components/NavHeader.tsx";
 import { SearchBar } from "#components/SearchBar.tsx";
 import SelectPostItLayer from "#components/SelectPostItLayer.tsx";
 import { Animal, getAllAnimals } from "../../data/dataBase/repository";
-import { useState, useEffect } from "react";
 import UnitPostItInfo from "#components/UnitPostItInfo.tsx";
 
 export function Animals() {
@@ -11,7 +11,11 @@ export function Animals() {
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredClass, setFilteredClass] = useState("");
   const [filteredAnimals, setFilteredAnimals] = useState<Animal[]>([]);
-  const [animals, setAnimals] = useState<Animal[]>(getAllAnimals())
+  const [animals, setAnimals] = useState<Animal[]>(getAllAnimals());
+
+  
+  const [backgroundImage, setBackgroundImage] = useState("./public/images/fondoMar.gif");
+  const [isFading, setIsFading] = useState(false);
 
   useEffect(() => {
     const results = animals.filter((animal) =>
@@ -19,47 +23,66 @@ export function Animals() {
       animal.class.toLowerCase().includes(filteredClass.toLowerCase())
     );
     setFilteredAnimals(results);
-  }, [searchTerm, animals]);
+  }, [searchTerm, filteredClass, animals]);
 
   function handleEvent(event: Event) {
     setIsDragging(event.type === "dragstart");
   }
 
-  useEffect(() => {
-    const results = animals.filter((animal) =>
-      animal.class.toLowerCase().includes(filteredClass.toLowerCase())
-    );
-    setFilteredAnimals(results);
-  }, [filteredClass, animals]);
+  function resetFilter() {
+    setFilteredClass("");
+    setFilteredAnimals(animals);
+    fadeBackground("./public/images/fondoMar.gif");
+  }
+
+  function fadeBackground(newImage: string) {
+    setIsFading(true);
+
+    setTimeout(() => {
+      setBackgroundImage(newImage);
+      setTimeout(() => setIsFading(false), 500); 
+    }, 500); 
+  }
 
   return (
     <SelectPostItLayer isDragging={isDragging}>
       <NavHeader pathItems={[{ nombre: "Animales", link: "/animales" }]} />
 
-      <div className="relative w-full h-5/6 flex items-center justify-center">
+      <div className="relative mt-5 w-full h-5/6 flex items-center justify-center">
         <img
-          src="./public/images/FondoMar.png"
-          alt="Fondo Mar"
-          className="absolute top-0 left-0 w-full h-full object-cover"
+          src={backgroundImage}
+          className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 ${isFading ? "opacity-0" : "opacity-100"}`}
         />
         <div className="relative z-10">
-          <NavBubbleAnimals setfiltered={setFilteredClass}/>
+          <NavBubbleAnimals setfiltered={setFilteredClass} setBackgroundImage={fadeBackground} />
         </div>
       </div>
+
       <span className="flex items-center justify-start gap-4 mt-8">
         <SearchBar estilo="w-1/3" onSearch={setSearchTerm} />
-        <h2 className="text-xl" >Filtrando por clase: {filteredClass}</h2>
+        <h2 className="text-xl">
+          Filtrando por clase: {filteredClass}
+          {filteredClass && (
+            <button onClick={resetFilter} className="ml-2 w-6 h-6 cursor-pointer">
+              <img className="h-full w-full mt-1.5" src="./Public/svgs/Close.svg" alt="Limpiar búsqueda" />
+            </button>
+          )}
+        </h2>
       </span>
+
       <AnimalGrip filteredAnimals={filteredAnimals} handleEvent={handleEvent} />
     </SelectPostItLayer>
   );
 }
+
 export default Animals;
 
-function AnimalGrip({filteredAnimals, handleEvent}:{ filteredAnimals: Animal[], handleEvent: (event: Event) => void }) {
+
+
+function AnimalGrip({ filteredAnimals, handleEvent }: { filteredAnimals: Animal[], handleEvent: (event: Event) => void }) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4 pb-24">
-      {filteredAnimals.length > 0 && filteredAnimals.slice(0,18).map((info: Animal, i) => (
+      {filteredAnimals.length > 0 && filteredAnimals.slice(0, 18).map((info: Animal, i) => (
         <article key={i} className="flex flex-col items-center justify-center shadow-xl rounded-b-xl overflow-hidden">
           <UnitPostItInfo
             key={info.content.title}
@@ -73,8 +96,4 @@ function AnimalGrip({filteredAnimals, handleEvent}:{ filteredAnimals: Animal[], 
       ))}
     </div>
   );
-
 }
-
-
-
