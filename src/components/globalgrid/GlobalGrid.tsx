9bf90872e90736be03e6614bc3b7947dfeed0afd
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-05-19 22:19:44 -0400

    feat: que se vea tambien el header de home en su elemento global

--- src/components/globalgrid/GlobalGrid.tsx
+++ src/components/globalgrid/GlobalGrid.tsx
@@ -11,32 +11,41 @@ import {
   GlobalWidgetupdater
 } from "#lib/gridStackLib/index.js";
 
-export function GlobalGrip(){
-    const { widgets } = useWidgetContext();
+export function GlobalGrip() {
+  const { widgets } = useWidgetContext();
 
-    const gridOptions: GridStackOptions = {
-        acceptWidgets: true,
-        float: true,
-        column: 16,
-        row: 16,
-        cellHeight: 5,
-        cellHeightUnit: "rem",
-        margin: 2,
-        children: widgets
-    };
-    const [initialOptions] = useState(gridOptions)
+  const gridOptions: GridStackOptions = {
+    acceptWidgets: true,
+    float: true,
+    column: 16,
+    row: 16,
+    cellHeight: 5,
+    cellHeightUnit: "rem",
+    margin: 2,
+    children: widgets
+  };
+  const [initialOptions] = useState(gridOptions)
 
-    return (
-      <div className="bgGrid">
+  return (
+    <div className="bgGrid">
+      <header className="bg-light-tertiary px-4 text-white flex items-center gap-4 h-20 fixed w-full z-50">
+        <img src="logo.svg" alt="Logo Awani" className="size-24" />
+        <div>
+          <h1 className="text-2lx font-semibold text-start">Wamma</h1>
+          <p className="text-sm">Aprendices del agua</p>
+        </div>
+      </header>
+      <div className="pt-20">
         <GridStackProvider initialOptions={initialOptions}>
-            <GridStackRenderProvider>
-                <GridStackRender componentMap={COMPONENT_MAP} />
-            </GridStackRenderProvider>
-            {/*<DebugInfo />*/}
-            <GlobalWidgetupdater/>
+          <GridStackRenderProvider>
+            <GridStackRender componentMap={COMPONENT_MAP} />
+          </GridStackRenderProvider>
+          {/*<DebugInfo />*/}
+          <GlobalWidgetupdater />
         </GridStackProvider>
       </div>
-    )
+    </div>
+  )
 }
 export default GlobalGrip
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,20 +5,11 @@ import GlobalGrip from "#components/globalgrid/GlobalGrid.tsx";
 export function Home() {
 
   return (
-    <div className="bgGrid">
-      <header className="bg-light-tertiary px-4 text-white flex items-center gap-4 h-20 fixed w-full z-50">
-        <img src="logo.svg" alt="Logo Awani" className="size-24" />
-        <div>
-          <h1 className="text-2lx font-semibold text-start">Wamma</h1>
-          <p className="text-sm">Aprendices del agua</p>
-        </div>
-      </header>
-      <div className="pt-20">
-        <GlobalGrip/>
-      </div>
+    <>
+      <GlobalGrip/>
       <NavMenu text="Menu"/>
       <TransitionToPage/>
-    </div>
+    </>
   );
 }
 export default Home;
